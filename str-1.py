932b4e0572606de6ebaff50847b1a9092bd191b1
Zac <a19860101@gmail.com>
2025-05-16 14:39:14 +0800

    20250516

--- str-1.py
+++ str-1.py
@@ -10,11 +10,20 @@ s = 'hEllO pYtHon'
 # print(s[2:])
 # print(s[:7])
 
+# 計算文字長度(文字數量包含空格)
 print(len(s))
 
+# 大寫
 print(s.upper())
+
+# 小寫
 print(s.lower())
+
+# 首字大寫
 print(s.capitalize())
+
+# 每個單字首字大寫
 print(s.title())
 
+#取代文字
 print(s.replace('pYtHon','World'))
