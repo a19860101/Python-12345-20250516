1e483a9d46408db5d263673a732df7103e974c74
Zac <a19860101@gmail.com>
2025-05-16 14:52:22 +0800

    20250516

--- str-1.py
+++ str-1.py
@@ -11,19 +11,42 @@ s = 'hEllO pYtHon'
 # print(s[:7])
 
 # 計算文字長度(文字數量包含空格)
-print(len(s))
+# print(len(s))
 
-# 大寫
-print(s.upper())
+# upper() 大寫
+# print(s.upper())
 
-# 小寫
-print(s.lower())
+# lower() 小寫
+# print(s.lower())
 
-# 首字大寫
-print(s.capitalize())
+# capitalize() 首字大寫
+# print(s.capitalize())
 
-# 每個單字首字大寫
-print(s.title())
+# title() 每個單字首字大寫
+# print(s.title())
 
-#取代文字
-print(s.replace('pYtHon','World'))
+# replace() 取代文字
+# print(s.replace('pYtHon','World'))
+
+# index() 取得字串索引值 若找不到會報錯
+q = 'hello world'
+# print(q.index('a'))
+
+# find() 取得字串索引值 若找不到會回傳-1
+# print(q.find('a'))
+
+# isalpha() 判斷字串是否為英文
+print(q.isalpha())
+# isdigit() 判斷字串是否為數字
+print(q.isdigit())
+q2 = '12345'
+print(q2.isdigit())
+# endswith() 判斷字串最後一個字母是否為指定字母
+print(q.endswith('a'))
+# startswith() 判斷字串第一個字母是否為指定字母
+print(q.startswith('s'))
+
+# split() 字串轉列表
+# strip() 去除前後空白
+# lstrip() 去除開頭空白
+# rstrip() 去除結尾空白
