c31495a52ad04b10cf7923e3b56f70f1754ec5cb
Zac <a19860101@gmail.com>
2025-05-16 11:44:56 +0800

    20250516

--- operator.py
+++ operator.py
@@ -1,14 +1,28 @@
 # 運算元 operator
 
 x = 10
+# 算術運算元
 print(x + 2)
 print(x - 2)
 print(x * 2)
 print(x / 2)
 print(x % 3) #求餘數
 
+# 比較運算元
 print(x > 0)
 print(x < 0)
 print(x >= 0)
 print(x <= 0)
 print(x == 0)
+
+# 指定運算元
+x += 2 # x = x + 2
+print(x) # 12
+x -= 2
+print(x) # 10
+x *= 2
+print(x) # 20
+x /= 2
+print(x) # 10
+x %= 2
+print(x) # 0
