e3ba416788fb61351045dff1fdb128c6fe773c25
Zac <a19860101@gmail.com>
2025-05-16 10:49:49 +0800

    20250516

---
+++ 001.py
@@ -0,0 +1,32 @@
+# 變數
+# 首字不可使用數字或特殊符號
+# 大小寫有別
+# 不可使用關鍵字(保留字)
+a = 100
+A = 'hello'
+
+import keyword
+# print(keyword.kwlist)
+
+# 資料型態
+
+# int integer 整數
+d1 = 0
+print(type(d1))
+
+# float 浮點數
+d2 = 2.3
+print(type(d2))
+
+# str string 字串
+d3 = 'Hello'
+print(type(d3))
+
+# bool boolean 布林值
+d4 = True
+d5 = False
+print(type(d4))
+
+# NoneType 無類型
+d6 = None
+print(type(d6))
