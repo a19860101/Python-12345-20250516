ad8b351a60a97279d51cbd79e49ee1c9b48424cc
Zac <a19860101@gmail.com>
2025-05-16 13:41:46 +0800

    20250516

--- statement.py
+++ statement.py
@@ -16,3 +16,22 @@ elif x < 0:
 else:
     print('0')
 
+# 3.10之後
+
+s = 'tue'
+
+match s:
+    case 'sun':
+        print('日')
+    case 'mon':
+        print('一')
+    case 'tue':
+        print('二')
+
+if s is 'sun':
+    print('日')
+elif s is 'mon':
+    print('一')
+elif s is 'tue':
+    print('二')
+
