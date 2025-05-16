aeee2d67d1615e9fd6c8d0c154a075f11c23174b
Zac <a19860101@gmail.com>
2025-05-16 13:08:56 +0800

    20250516

--- operator.py
+++ operator.py
@@ -2,31 +2,41 @@
 
 x = 10
 # 算術運算元
-print(x + 2)
-print(x - 2)
-print(x * 2)
-print(x / 2)
-print(x % 3) #求餘數
-print(x ** 10)
-print(x // 3)
+# print(x + 2)
+# print(x - 2)
+# print(x * 2)
+# print(x / 2)
+# print(x % 3) #求餘數
+# print(x ** 10)
+# print(x // 3)
 
 # 比較運算元
-print(x > 0)
-print(x < 0)
-print(x >= 0)
-print(x <= 0)
-print(x == 0)
+# print(x > 0)
+# print(x < 0)
+# print(x >= 0)
+# print(x <= 0)
+# print(x == 0)
 
 # 指定運算元
 x += 2 # x = x + 2
-print(x) # 12
+# print(x) # 12
 x -= 2
-print(x) # 10
+# print(x) # 10
 x *= 2
-print(x) # 20
+# print(x) # 20
 x /= 2
-print(x) # 10
+# print(x) # 10
 x %= 2
-print(x) # 0
+# print(x) # 0
 x **= 2
 x //= 2
+
+# 邏輯
+a = 10
+b = -2
+print(a > 0 and b > 0)
+print(a > 0 or b > 0)
+
+# 身分
+print( a is 2 )
+print( a is not 2)
