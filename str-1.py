3fe003f15b1d8406666135b81acb921e306dd252
Zac <a19860101@gmail.com>
2025-05-16 14:37:56 +0800

    20250516

--- str-1.py
+++ str-1.py
@@ -1,11 +1,20 @@
-s = 'hello python'
+s = 'hEllO pYtHon'
 
-print(s[0])
-print(s[1])
-print(s[-1])
-print(s[-2])
+# print(s[0])
+# print(s[1])
+# print(s[-1])
+# print(s[-2])
+#
+# print(s[2:7])
+# print(s[3:-1])
+# print(s[2:])
+# print(s[:7])
 
-print(s[2:7])
-print(s[3:-1])
-print(s[2:])
-print(s[:7])
+print(len(s))
+
+print(s.upper())
+print(s.lower())
+print(s.capitalize())
+print(s.title())
+
+print(s.replace('pYtHon','World'))
