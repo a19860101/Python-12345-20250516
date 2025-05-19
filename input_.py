306e4978b97b6457106f55a852ed996cdde51c7e
Zac <a19860101@gmail.com>
2025-05-19 09:40:43 +0800

    20250519

--- input_.py
+++ input_.py
@@ -1,3 +1,13 @@
-q = input('請輸入文字:')
+# q = input('請輸入文字:')
+#
+# print(q)
 
-print(q)
+# 台幣轉美金
+
+x = input('請輸入台幣:')
+rate = 30.2
+
+# result = int(x) / rate
+result = int(x) // rate
+
+print(f'{x}元台幣約等於{result}美金')
