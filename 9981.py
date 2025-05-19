08354e287cf434115030451d3df36991edbba6e5
Zac <a19860101@gmail.com>
2025-05-19 09:04:42 +0800

    20250516

--- 9981.py
+++ 9981.py
@@ -1,5 +1,16 @@
 # 九九乘法表
+#
+# for i in range(2,10):
+#     for j in range(1,10):
+#         print(f'{i}x{j}={i*j}')
 
-for i in range(2,10):
-    for j in range(1,10):
-        print(f'{i}x{j}={i*j}')
+
+# for i in range(1,5):
+#     print('*'*i)
+
+for i in range(10):
+    for a in range(1,10-i):
+        print('-' * a)
+    for b in range(10-i,10):
+        print('Q' * b)
+    print('\n', end='')
