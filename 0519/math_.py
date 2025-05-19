af325fa9860b8138df518fed64418bb3be0a7aad
Zac <a19860101@gmail.com>
2025-05-19 11:31:16 +0800

    20250519

---
+++ 0519/math_.py
@@ -0,0 +1,18 @@
+
+# print(round(3.14))
+# print(max(12,24,55))
+# print(min(12,24,55))
+
+# 絕對值
+# print(abs(-24))
+
+import math
+
+#無條件捨去
+r = math.floor(3.4)
+# 無條件進位
+r2 = math.ceil(3.4)
+
+print(math.pi)
+
+print(round(math.pi,2))
