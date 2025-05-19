a2228b14f9da3a99de50f69e8b079c3d49b63d8e
Zac <a19860101@gmail.com>
2025-05-19 14:32:56 +0800

    20250519

--- 0519/list-1.py
+++ 0519/list-1.py
@@ -3,9 +3,13 @@
 # a = list()
 # a = []
 
-a = ['Apple', 123, 3.12, True]
-print(a)
-print(a[0])
-print(a[1])
-print(a[2])
-print(a[3])
+a = ['Apple', 123, 3.12, True, True, False, 999]
+# print(a)
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[3])
+
+for data in a:
+    print(data)
+
