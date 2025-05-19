1debc44c5c246ab8f453c700fd2a73caf71220ea
Zac <a19860101@gmail.com>
2025-05-19 09:55:09 +0800

    20250519

--- input_.py
+++ input_.py
@@ -7,7 +7,10 @@
 x = input('請輸入台幣:')
 rate = 30.2
 
-# result = int(x) / rate
-result = int(x) // rate
+result = int(x) / rate
+result = round(result, 2)
+# result = round(int(x) / rate,2)
+
+# result = int(x) // rate
 
 print(f'{x}元台幣約等於{result}美金')
