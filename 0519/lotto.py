b32fa888cbeeea7179c29d6921958a5b6519f6f0
Zac <a19860101@gmail.com>
2025-05-19 16:14:49 +0800

    20250519

--- 0519/lotto.py
+++ 0519/lotto.py
@@ -1,4 +1,6 @@
+# 大樂透電腦選號
 # 大樂透 1-49 7
+
 
 import random
 
@@ -12,3 +14,8 @@ while len(result) < 7:
 print(result)
 print(f'大樂透號碼為{result[:6]}特別號為{result[-1]}')
 print(f'大樂透號碼為{result[1:]}特別號為{result[0]}')
+
+r = random.sample(range(1,50), 7)
+print(r)
+print(f'大樂透號碼為{r[:6]}特別號為{r[-1]}')
+print(f'大樂透號碼為{r[1:]}特別號為{r[0]}')
