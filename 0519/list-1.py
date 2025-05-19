ee1110127dbff2a0ee37973fc47068dee4144027
Zac <a19860101@gmail.com>
2025-05-19 14:46:00 +0800

    20250519

--- 0519/list-1.py
+++ 0519/list-1.py
@@ -3,13 +3,30 @@
 # a = list()
 # a = []
 
-a = ['Apple', 123, 3.12, True, True, False, 999]
+a = ['Apple', 123, 3.12,True , True, True, False, 999]
 # print(a)
 # print(a[0])
 # print(a[1])
 # print(a[2])
 # print(a[3])
 
-for data in a:
-    print(data)
+# for data in a:
+#     print(data)
+
+# len()
+print(len(a))
+
+# index()
+print(a.index(123))
+
+# count()
+print(a.count(True))
+
+# append()
+a.append('hello')
+print(a)
+
+# extend()
+a.extend(['hello','Python'])
+print(a)
 
