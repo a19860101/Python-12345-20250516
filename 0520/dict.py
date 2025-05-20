7f560f2ebfe6b9294498fde41ee92c374be1978a
Zac <a19860101@gmail.com>
2025-05-20 09:55:30 +0800

    20250520

--- 0520/dict.py
+++ 0520/dict.py
@@ -8,12 +8,22 @@
 users = {
     'name': 'John',
     'email': 'asdf@gmail.com',
-    'phone': '0987654321'
+    'phone': '0987654321',
+    'age': '30'
 }
 
+# print(users)
+# print(users['name'])
+# print(users['email'])
+# print(users['phone'])
+# print(users.keys())
+# print(users.values())
+# print(users.items())
+
+# users['age'] = '20'
+# users.setdefault('age','20')
+
+# del users['email']
+users.pop('age')
+
 print(users)
-print(users['name'])
-print(users['email'])
-print(users['phone'])
-print(users.keys())
-print(users.values())
