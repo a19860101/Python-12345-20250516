7a0b807a2b5cbbe72db6a70975f01b0a44fcc398
Zac <a19860101@gmail.com>
2025-05-20 09:25:24 +0800

    20250520

---
+++ 0520/list-2.py
@@ -0,0 +1,17 @@
+# 串列
+
+datas = [12,24,35,66,15,36,39]
+
+# sort()
+# datas.sort()
+# datas.sort(reverse=True)
+# print(datas)
+
+# reverse()
+# datas.reverse()
+# print(datas)
+
+# copy()
+# datas2 = datas.copy()
+datas2 = datas
+print(datas2)
