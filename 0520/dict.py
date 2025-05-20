5150830c179228a86837f6155d289ddc0ac2d765
Zac <a19860101@gmail.com>
2025-05-20 09:40:45 +0800

    20250520

---
+++ 0520/dict.py
@@ -0,0 +1,19 @@
+# dictionary 字典
+
+# users = {
+#     'key':'value',
+#     'key1':'value1'
+# }
+
+users = {
+    'name': 'John',
+    'email': 'asdf@gmail.com',
+    'phone': '0987654321'
+}
+
+print(users)
+print(users['name'])
+print(users['email'])
+print(users['phone'])
+print(users.keys())
+print(users.values())
