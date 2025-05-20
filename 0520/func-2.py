52ef272c1ebd869141d69a39fb4513a77470e024
Zac <a19860101@gmail.com>
2025-05-20 15:42:27 +0800

    20250520

--- 0520/func-2.py
+++ 0520/func-2.py
@@ -5,7 +5,26 @@ def foo(*args):
 def foo2(*args):
     return args
 
-datas = foo2('apple',123,'banana',999)
+# datas = foo2('apple',123,'banana',999)
 
-for data in datas:
-    print(data)
+# for data in datas:
+#     print(data)
+
+def foo3(**kwargs):
+    return kwargs
+
+# datas = foo3(name='John',email='asdf@gmail.com')
+# for k,v in datas.items():
+#     print(f'{k}:{v}')
+
+def foo4(**kwargs):
+    for k,v in kwargs.items():
+        print(f'{k}:{v}')
+
+# foo4(name='John',email='asdf@gmail.com',phone='98798798798')
+
+def foo5(*args, **kwargs):
+    return [args, kwargs]
+
+r = foo5(1,2,3,name='john')
+print(r)
