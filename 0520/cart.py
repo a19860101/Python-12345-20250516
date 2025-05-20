cdf679a5bf368d926056ee5d6aeaeb5c9bb9965f
Zac <a19860101@gmail.com>
2025-05-20 11:39:40 +0800

    20250520

--- 0520/cart.py
+++ 0520/cart.py
@@ -11,3 +11,14 @@ print('-----商品項目-----')
 for k,v in products.items():
     print(f'{k:10}: {v}元')
 print('---------------')
+
+while True:
+    selected = input('請輸入商品名稱(按q完成購買):')
+    if selected == 'q':
+        break
+    elif selected not in products:
+        print('沒有這個商品')
+    elif selected in products:
+        cart.append(selected)
+
+print(cart)
