6a277afed02583d8b98f79ab506f5590332e2b77
Zac <a19860101@gmail.com>
2025-05-21 10:50:54 +0800

    20250521

--- 0521/002.py
+++ 0521/002.py
@@ -1,10 +1,15 @@
 # import PIL
 from PIL import Image
 import glob
+import os
 
 image = glob.glob('../images/*.[Jj][Pp][Gg]')
 
 # print(image[0])
 img = Image.open(image[0])
 
-print(img)
+os.makedirs('images', exist_ok=True)
+
+img.save('images/test.jpg',quality=50, subsampling=0)
+# quality 0-100
+# subsampling 0,1,2
