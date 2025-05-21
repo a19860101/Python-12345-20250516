47c06e4480e0f3631bf0e6109760be406a3f3edd
Zac <a19860101@gmail.com>
2025-05-21 11:18:07 +0800

    20250521

--- 0521/002.py
+++ 0521/002.py
@@ -5,11 +5,21 @@ import os
 
 image = glob.glob('../images/*.[Jj][Pp][Gg]')
 
+# 單一照片壓縮
 # print(image[0])
-img = Image.open(image[0])
-
-os.makedirs('images', exist_ok=True)
-
-img.save('images/test.jpg',quality=50, subsampling=0)
+# img = Image.open(image[0])
+#
+# os.makedirs('images', exist_ok=True)
+#
+# img.save('images/test.jpg',quality=50, subsampling=0)
 # quality 0-100
 # subsampling 0,1,2
+
+# 批次圖片壓縮
+
+for idx,img in enumerate(image):
+    im = Image.open(img)
+    print(im)
+    os.makedirs('images', exist_ok=True)
+    im.save(f'images/img-{idx+1}.jpg', quality=50, subsampling=0)
+
