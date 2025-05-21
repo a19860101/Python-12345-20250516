768ebe4316e63bbaadc381b78d4776509c28f918
Zac <a19860101@gmail.com>
2025-05-21 13:16:20 +0800

    20250521

--- 0521/003.py
+++ 0521/003.py
@@ -7,8 +7,20 @@ images = glob.glob('../images/*.[Jj][Pp][Gg]')
 
 image = Image.open(images[0])
 
-small_image = image.resize((400,900))
+# print(image.size)
+# print(image.size[0])
+# print(image.size[1])
+
+w = image.size[0]
+h = image.size[1]
+
+resize_w = 1200
+resize_h = int(h * resize_w / w)
+
+print(resize_h)
+
+small_image = image.resize((resize_w,resize_h))
 
 os.makedirs('images',exist_ok=True)
 
-small_image.save('images/small.jpg')
+small_image.save('images/small.jpg',quality=60, subsampling=0)
