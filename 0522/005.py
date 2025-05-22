ea70fe9e13f621dd0a82f20b1030942d8dfa4eae
Zac <a19860101@gmail.com>
2025-05-22 10:51:14 +0800

    20250522

--- 0522/005.py
+++ 0522/005.py
@@ -2,12 +2,8 @@ from PIL import Image, ImageDraw
 
 img = Image.open('../images/19-2500x1667.jpg')
 
-w, h = img.size
-
-# print(w,h)
-
 draw = ImageDraw.Draw(img)
 
-draw.circle((w/2,h/2),radius=200, outline='white', width=20)
+draw.polygon([(100,100),(1800,1200),(1600,900),(10,1200)],outline='red', width=10, fill='white')
 
 img.show()
