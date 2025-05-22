70e39a305a7137f491dc607a9ef09f6a9259229b
Zac <a19860101@gmail.com>
2025-05-22 13:49:52 +0800

    20250522

---
+++ 0521/practice.py
@@ -0,0 +1,33 @@
+# 修改圖片尺寸
+import os
+import glob
+from PIL import Image
+
+images = glob.glob('./images/*.[Jj][Pp][Gg]')
+# 打包的路徑
+# images = glob.glob('./images/*.[Jj][Pp][Gg]')
+resize_w = int(input('請輸入縮圖寬度:'))
+qu = int(input('請輸入品質(0-100):'))
+deg = int(input('請輸入旋轉角度:'))
+
+for idx, image in enumerate(images):
+    img = Image.open(image)
+
+    # print(image.size)
+    # print(image.size[0])
+    # print(image.size[1])
+
+    w = img.size[0]
+    h = img.size[1]
+
+    resize_h = int(h * resize_w / w)
+
+    print(resize_h)
+
+    small_image = img.resize((resize_w,resize_h))
+
+    small_image = small_image.rotate(deg, expand=1)
+
+    os.makedirs('small',exist_ok=True)
+
+    small_image.save(f'small/small-{idx + 1}.jpg',quality=qu, subsampling=1)
