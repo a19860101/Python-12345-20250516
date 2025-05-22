3bf95fd12c4ebcfcaa53d7355aad7cce23a7caee
Zac <a19860101@gmail.com>
2025-05-22 13:31:45 +0800

    20250522

--- 0522/007.py
+++ 0522/007.py
@@ -1,13 +1,15 @@
 from PIL import Image, ImageFont, ImageDraw
 
-img = Image.open('../images/19-2500x1667.jpg')
+img = Image.open('../images/19-2500x1667.jpg').convert('RGBA')
 w,h = img.size
-text = Image.new(mode='RGBA', size=img.size, color=(255,0,0,255))
+text = Image.new(mode='RGBA', size=img.size, color=(255,0,0,0))
 
 font = ImageFont.truetype('../font/Anton-Regular.ttf', 100)
 
 draw = ImageDraw.Draw(text)
 
-draw.text((0,0),'HELLO', font=font, fill='white')
+draw.text((0,0),'HELLO', font=font, fill=(255,255,255,100))
 
-text.show()
+result = Image.alpha_composite(img, text)
+
+result.show()
