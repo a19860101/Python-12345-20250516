2f34745a50a0ce29362a68e92ceddc33202d20a9
Zac <a19860101@gmail.com>
2025-05-23 13:17:40 +0800

    20250523

--- 0523/005.py
+++ 0523/005.py
@@ -12,5 +12,18 @@ t4 = 1,2,3,4,5
 t4 = list(t4)
 t4 = tuple(t4)
 
-print(type(t4))
+# print(type(t4))
+
+# set 集合
+
+s1 = {1,2,3,4,2,2,2,2,3,3,4,5,1,2,3,4}
+
+s2 = {'apple','apple','banana'}
+
+s3 = [1,2,3,4,5,5,4,3,2,1]
+
+s3 = set(s3)
+
+print(s3)
+
 
