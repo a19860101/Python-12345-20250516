e62ed39325c1a7c37b8188c15c9c7640b031bc70
Zac <a19860101@gmail.com>
2025-05-23 15:25:08 +0800

    20250523

--- 0523/crawler-3.py
+++ 0523/crawler-3.py
@@ -10,6 +10,9 @@ header = {
 }
 
 request = req.Request(url, headers=header)
+"""
+使用with...as的res變數會自動關閉
+"""
 
 # res = req.urlopen(request)
 #
@@ -19,7 +22,9 @@ request = req.Request(url, headers=header)
 
 with req.urlopen(request) as res:
     result = res.read().decode('utf-8')
-
+"""
+使用 with...as 時 res 會自動關閉，不用手動關閉 res.close()
+"""
 # print(result)
 
 if res.closed:
---
+++ 0523/crawler-4.py
@@ -0,0 +1,7 @@
+# Beautiful Soup
+# 安裝套件 pip install beautifulsoup4
+
+import urllib.request as req
+import bs4
+
+# from bs4 import BeautifulSoup
