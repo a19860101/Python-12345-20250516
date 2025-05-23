f7a4d7fd28c1c3bfe540943bf2b80741e2ac8a42
Zac <a19860101@gmail.com>
2025-05-23 15:39:08 +0800

    20250523

--- 0523/crawler-4.py
+++ 0523/crawler-4.py
@@ -5,3 +5,23 @@ import urllib.request as req
 import bs4
 
 # from bs4 import BeautifulSoup
+
+url = 'https://www.ptt.cc/bbs/NBA/index.html'
+
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
+    'cookie': 'over18=1',
+}
+
+request = req.Request(url, headers=header)
+
+with req.urlopen(request) as res:
+    result = res.read().decode('utf-8')
+
+source = bs4.BeautifulSoup(result, 'html.parser')
+
+s = source.find('a',class_='board')
+logo = source.find('a', id='logo')
+
+print(logo.string)
+
