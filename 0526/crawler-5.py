b6952327d1a7ca41f71843a0cd7dd9d115d8526f
Zac <a19860101@gmail.com>
2025-05-26 13:42:31 +0800

    20250526

--- 0526/crawler-5.py
+++ 0526/crawler-5.py
@@ -4,10 +4,16 @@ import ssl
 import json
 import os
 
+wb = openpyxl.Workbook()
+ws = wb.active
+
+title = ['名稱','地址']
+ws.append(title)
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# url = 'https://media.taiwan.net.tw/XMLReleaseALL_public/hotel_C_f.json'
-url = 'https://data.moa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&IsTransData=1'
+url = 'https://media.taiwan.net.tw/XMLReleaseALL_public/hotel_C_f.json'
+# url = 'https://data.moa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&IsTransData=1'
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
 }
@@ -15,6 +21,19 @@ header = {
 request = req.Request(url, headers=header)
 
 with req.urlopen(request) as res:
-    result = res.read().decode('utf-8')
+    result = res.read().decode('utf-8-sig')
 
 print(result)
+
+json_datas = json.loads(result)
+
+# print(json_datas)
+
+os.makedirs('output', exist_ok=True)
+
+for data in json_datas['XML_Head']['Infos']['Info']:
+    print(data)
+    ct = [data['Name'],data['Add']]
+    ws.append(ct)
+
+wb.save('output/hotel.xlsx')
