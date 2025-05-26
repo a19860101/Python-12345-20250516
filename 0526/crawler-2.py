9d4f57661040db6700bb85d73a82e8c9b2867704
Zac <a19860101@gmail.com>
2025-05-26 10:56:37 +0800

    20250526

--- 0526/crawler-2.py
+++ 0526/crawler-2.py
@@ -7,7 +7,7 @@ import os
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-url = 'https://www.kkday.com/zh-tw/category/ajax_get_category_product_list?productCategory=CATEGORY_019&destination=D-JP-3231&keyword=&currency=TWD&sort=prec&page=1&start=0&count=10'
+url = 'https://www.kkday.com/zh-tw/category/ajax_get_category_product_list?productCategory=CATEGORY_019&destination=D-JP-3231&keyword=&currency=TWD&sort=prec&page=1&start=0&count=275'
 
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
