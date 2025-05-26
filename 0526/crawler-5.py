864a2c7739ed32fd588eb52cfb2ea4974911419d
Zac <a19860101@gmail.com>
2025-05-26 13:17:59 +0800

    20250526

---
+++ 0526/crawler-5.py
@@ -0,0 +1,20 @@
+import urllib.request as req
+import openpyxl
+import ssl
+import json
+import os
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# url = 'https://media.taiwan.net.tw/XMLReleaseALL_public/hotel_C_f.json'
+url = 'https://data.moa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&IsTransData=1'
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
+}
+
+request = req.Request(url, headers=header)
+
+with req.urlopen(request) as res:
+    result = res.read().decode('utf-8')
+
+print(result)
