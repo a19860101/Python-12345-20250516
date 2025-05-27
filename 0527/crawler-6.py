b8e85e94eb0361b60fb82adc8868b13a9346b3a9
Zac <a19860101@gmail.com>
2025-05-27 15:37:17 +0800

    20250527

--- 0527/crawler-6.py
+++ 0527/crawler-6.py
@@ -10,13 +10,14 @@ res = requests.get(url, verify=False)
 
 htmlfile = bs4.BeautifulSoup(res.text, 'html.parser')
 
-imgs = htmlfile.find_all('img')
-
+# imgs = htmlfile.find_all('img')
+imgs = htmlfile.select('body > article > ul > li > figure > a > img')
+# body > article > ul > li:nth-child(1) > figure > a > img
 for idx,img in enumerate(imgs):
-    # print(img['src'])
+    # print(img['title'])
     img_url = urljoin(url, img['src'])
-    print(img_url)
+    # print(img_url)
     im = requests.get(img_url, verify=False)
     os.makedirs('movie', exist_ok=True)
-    with open(f'movie/{idx}.jpg', 'wb')as f:
+    with open(f'movie/{img['title']}.jpg', 'wb')as f:
         f.write(im.content)
