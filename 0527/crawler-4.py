372f728de12c7b9c70c6d6d4eb08b9622574e613
Zac <a19860101@gmail.com>
2025-05-27 13:44:55 +0800

    20250527

---
+++ 0527/crawler-4.py
@@ -0,0 +1,22 @@
+import os
+
+import bs4
+import requests
+
+url = 'https://www.tenlong.com.tw/'
+
+res = requests.get(url)
+
+htmlfile = bs4.BeautifulSoup(res.text, 'html.parser')
+
+imgs = htmlfile.find_all('img')
+
+for idx,img in enumerate(imgs):
+    # print(img['src'])
+    try:
+        img_src = requests.get(img['src'])
+        os.makedirs('tenlong', exist_ok=True)
+        with open(f'tenlong/{idx}.jpg', 'wb')as f:
+            f.write(img_src.content)
+    except:
+        pass
