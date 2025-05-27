b18cb88f21aacbfd9ed1eadd6fe6434268b91d0f
Zac <a19860101@gmail.com>
2025-05-27 15:21:47 +0800

    20250527

---
+++ 0527/crawler-6.py
@@ -0,0 +1,22 @@
+import os
+from urllib.parse import urljoin
+
+import bs4
+import requests
+
+url = 'https://www.vscinemas.com.tw/film/index.aspx'
+
+res = requests.get(url, verify=False)
+
+htmlfile = bs4.BeautifulSoup(res.text, 'html.parser')
+
+imgs = htmlfile.find_all('img')
+
+for idx,img in enumerate(imgs):
+    # print(img['src'])
+    img_url = urljoin(url, img['src'])
+    print(img_url)
+    im = requests.get(img_url, verify=False)
+    os.makedirs('movie', exist_ok=True)
+    with open(f'movie/{idx}.jpg', 'wb')as f:
+        f.write(im.content)
