9d81dc1fe458274104c154f6003223e25928d207
Zac <a19860101@gmail.com>
2025-05-28 10:38:50 +0800

    20250528

--- 0528/selenium-3.py
+++ 0528/selenium-3.py
@@ -1,17 +1,34 @@
 import time
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
 driver.get('https://google.com')
-
+time.sleep(3)
 driver.maximize_window()
+time.sleep(2)
 
 search = driver.find_element(By.CLASS_NAME, 'gLFyf')
-search.send_keys('Python')
+# search.send_keys('Python', Keys.ENTER)
+search.send_keys('P')
+time.sleep(1)
+search.send_keys('y')
+time.sleep(2)
+search.send_keys('t')
+time.sleep(1)
+search.send_keys('h')
+time.sleep(3)
 
+search.send_keys('o')
+time.sleep(2)
+search.send_keys('n')
 time.sleep(5)
+
+search.send_keys(Keys.ENTER)
+
+time.sleep(15)
 
 
 driver.close()
