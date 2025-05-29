2beac66fe60ebba9b6b38d4c1c65e0fd0d3ddafb
Zac <a19860101@gmail.com>
2025-05-29 13:33:07 +0800

    20250529

--- 0529/selenium-uniqlo-1.py
+++ 0529/selenium-uniqlo-1.py
@@ -31,6 +31,17 @@ sale = driver.find_element(By.CSS_SELECTOR,'img[alt="特價商品"]')
 
 sale.click()
 
-time.sleep(10)
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, 'product-ul'))
+)
+
+time.sleep(2)
+
+products = driver.find_elements(By.CLASS_NAME, 'product-li')
+
+for product in products:
+    title = product.find_element(By.CLASS_NAME, 'ec-font-sub-title').text
+    price = product.find_element(By.CLASS_NAME, 'h-currency').text
+    print(f'{title}:{price}')
 
 driver.close()
