e4197f7b657b7898227723544f08728d966d9379
Zac <a19860101@gmail.com>
2025-05-29 15:32:42 +0800

    20250529

--- 0529/selenium-uniqlo-1.py
+++ 0529/selenium-uniqlo-1.py
@@ -1,7 +1,9 @@
 import time
 
+import openpyxl
 from selenium import webdriver
 from selenium.common import TimeoutException
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
@@ -12,25 +14,27 @@ url = 'https://www.uniqlo.com/tw/zh_TW/'
 driver = webdriver.Chrome()
 driver.get(url)
 driver.maximize_window()
+action = ActionChains(driver)
 
 time.sleep(3)
 
 # search = driver.find_element(By.XPATH, '//*[@id="hmall-container"]/div/div[1]/div[3]/div[4]/div/ul/li[2]')
 search = driver.find_element(By.CLASS_NAME, 'li-search')
 
-search.click()
-
-time.sleep(5)
+# search.click()
+action.click(search).pause(5).perform()
+# time.sleep(5)
 
 ck = driver.find_element(By.ID, 'onetrust-accept-btn-handler')
 
-ck.click()
-
-time.sleep(3)
+# ck.click()
+# time.sleep(3)
+action.click(ck).pause(3).perform()
 
 sale = driver.find_element(By.CSS_SELECTOR,'img[alt="特價商品"]')
 
-sale.click()
+# sale.click()
+action.click(sale).perform()
 
 WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CLASS_NAME, 'product-ul'))
@@ -54,9 +58,18 @@ while True:
 
 products = driver.find_elements(By.CLASS_NAME, 'product-li')
 
+# wb = openpyxl.Workbook()
+# ws = wb.active
+# ws.title = '女裝'
+
+# ws.append(['商品名稱','代碼','價格','圖片連結'])
+
 for product in products:
     title = product.find_element(By.CLASS_NAME, 'ec-font-sub-title').text
     price = product.find_element(By.CLASS_NAME, 'h-currency').text
-    print(f'{title}:{price}')
-
+    img = product.find_element(By.CLASS_NAME, 'picture-img')
+    im = img.get_property('src')
+    # print(f'{title}:{price}')
+    # ws.append([title[:-6],title[-6:],price,im])
+# wb.save('u.xlsx')
 driver.close()
