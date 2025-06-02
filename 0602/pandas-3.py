26570df2f517b670e32a1ded8cc1f06deca800e1
Zac <a19860101@gmail.com>
2025-06-02 15:17:31 +0800

    20250602

--- 0602/pandas-3.py
+++ 0602/pandas-3.py
@@ -2,13 +2,15 @@ import pandas as pd
 
 datas = pd.read_csv('./Restaurant_C_f.csv')
 # print(datas)
-# print(datas.columns)
+print(datas.columns)
 # print(datas['Name'])
 # print(datas.loc[1])
 
 condition = datas['Add'].str.contains('中壢')
-# condition = datas['Town'].str.contains('中壢')
 result = datas[condition]
 
-# print(result)
+# result = result[['Name', 'Add']]
+
+result = result.drop(['Id', 'Description'], axis=1)
+
 result.to_excel('a.xlsx')
