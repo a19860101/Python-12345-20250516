c7d3f11084532efb4b811a366ca0eaa27f97fe0f
Zac <a19860101@gmail.com>
2025-06-02 10:26:38 +0800

    20250602

--- 0602/pandas-1.py
+++ 0602/pandas-1.py
@@ -3,7 +3,7 @@ import pandas as pd
 # 一維資料
 data = pd.Series([12,35,26,43,71])
 
-print(data)
+# print(data)
 
 # 二維資料
 # data2 = pd.DataFrame([12,35,26,43,71])
@@ -21,12 +21,28 @@ data4 = pd.DataFrame([
 data5 = pd.DataFrame([
     {
         '名稱':'商品一',
-        '價格': '299'
+        '價格': '299',
+        '庫存': 2
     },
     {
         '名稱':'商品二',
-        '價格':'499'
-    }
-])
+        '價格':'499',
+        '庫存': 6
+    },
+    {
+        '名稱':'商品三',
+        '價格':'999',
+        '庫存': 12
+    },
+    {
+        '名稱':'商品四',
+        '價格':'999',
+        '庫存': 1
+    },
+], index=['A','B','C','D'])
 
 print(data5)
+print(data5.size)
+print(data5.columns)
+print(data5.index)
+print(data5.shape)
