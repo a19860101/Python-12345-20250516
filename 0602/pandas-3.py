f94c913f65347a8edb28b41364443772e2f09934
Zac <a19860101@gmail.com>
2025-06-02 15:38:26 +0800

    20250602

--- 0602/pandas-3.py
+++ 0602/pandas-3.py
@@ -6,13 +6,16 @@ print(datas.columns)
 # print(datas['Name'])
 # print(datas.loc[1])
 
-condition = datas['Add'].str.contains('中壢')
+# condition = datas['Add'].str.contains('中壢')
+condition = datas['Town'].str.contains('中壢').fillna(False)
 result = datas[condition]
 
-# 保留需要的欄位
-# result = result[['Name', 'Add']]
+# print(result)
 
+# 保留需要的欄位
+# result = result[['Name', 'Add', 'Town']]
+# print(result)
 # 移除不要的欄位
-result = result.drop(['Id', 'Description'], axis=1)
+# result = result.drop(['Id', 'Description'], axis=1)
 
 result.to_excel('a.xlsx')
