94b7e7c1e226cb4288a5b5d1f6b7b8e7e01ee957
Zac <a19860101@gmail.com>
2025-06-02 14:26:52 +0800

    20250602

--- 0602/pandas-3.py
+++ 0602/pandas-3.py
@@ -5,3 +5,10 @@ datas = pd.read_csv('./Restaurant_C_f.csv')
 # print(datas.columns)
 # print(datas['Name'])
 # print(datas.loc[1])
+
+condition = datas['Add'].str.contains('中壢')
+# condition = datas['Town'].str.contains('中壢')
+result = datas[condition]
+
+# print(result)
+result.to_excel('a.xlsx')
