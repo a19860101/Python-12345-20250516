fab7ed0d856ea3dbe8d73781723325e38b097e25
Zac <a19860101@gmail.com>
2025-06-03 14:22:46 +0800

    20250603

--- 0603/matplotlib-plot-5.py
+++ 0603/matplotlib-plot-5.py
@@ -1,1 +1,9 @@
-'https://api.openweathermap.org/data/2.5/forecast?q=taipei&appid=b1ecbccd638b763d489602917ba47cc3&units=metric&lang=zh_TW'
+import requests
+url = 'https://api.openweathermap.org/data/2.5/forecast?q=taipei&appid=b1ecbccd638b763d489602917ba47cc3&units=metric&lang=zh_TW'
+
+res = requests.get(url)
+
+result = res.json()
+
+for data in result['list']:
+    print(data)
