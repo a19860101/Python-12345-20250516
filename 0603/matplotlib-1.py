1f6e871657c060759ad45798daa017a2c029076c
Zac <a19860101@gmail.com>
2025-06-03 09:41:32 +0800

    20250603

--- 0603/matplotlib-1.py
+++ 0603/matplotlib-1.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 
 data1 = [20, 24, 32, 18, 26]
 
-plt.plot(data1)
+plt.plot(data1, color='red', linewidth=1, linestyle=':')
+# -, --, -. , :
 # plt.plot([20, 24, 32, 18, 26])
 
 plt.show()
