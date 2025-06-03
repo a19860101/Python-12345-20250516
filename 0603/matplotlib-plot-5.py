fde7f13bf08d0bc1db326141237bb71f889f037d
Zac <a19860101@gmail.com>
2025-06-03 15:36:21 +0800

    20250603

--- 0603/matplotlib-plot-5.py
+++ 0603/matplotlib-plot-5.py
@@ -9,17 +9,22 @@ res = requests.get(url)
 result = res.json()
 
 temp = []
+x = []
 
 for data in result['list']:
     # print(data['main']['temp'])
     temp.append(data['main']['temp'])
-
+    d = datetime.datetime.fromtimestamp(data['dt'])
+    x.append(f'{d.day}日{d.hour}時')
+print(x)
 plt.rc('font', family='Microsoft Jhenghei')
 
-plt.plot(temp,marker='.', markersize='6')
+plt.plot(x,temp,marker='.', markersize='6')
 plt.ylim(0,50)
 
 plt.title('天氣預報')
 plt.ylabel('溫度(攝氏)')
+
+# plt.xticks(x)
 
 plt.show()
