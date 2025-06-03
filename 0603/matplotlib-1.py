42ad4ecb4e30508862f6b2b7ba8a9c11cd195a18
Zac <a19860101@gmail.com>
2025-06-03 09:45:07 +0800

    20250603

--- 0603/matplotlib-1.py
+++ 0603/matplotlib-1.py
@@ -3,7 +3,15 @@ import matplotlib.pyplot as plt
 data1 = [20, 24, 32, 18, 26]
 
 plt.plot(data1, color='red', linewidth=1, linestyle=':')
-# -, --, -. , :
+
 # plt.plot([20, 24, 32, 18, 26])
 
 plt.show()
+
+"""
+
+color: 折線顏色
+linestyle: 折線樣式 (-, --, -., :)
+linewidth: 折線寬度
+
+"""
