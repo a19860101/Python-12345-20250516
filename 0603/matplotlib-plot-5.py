e8da25c1d5f78fc0458a9d39ac2e4c5f842c2fad
Zac <a19860101@gmail.com>
2025-06-03 14:38:18 +0800

    20250603

--- 0603/matplotlib-plot-5.py
+++ 0603/matplotlib-plot-5.py
@@ -1,9 +1,24 @@
 import requests
+import matplotlib.pyplot as plt
+
 url = 'https://api.openweathermap.org/data/2.5/forecast?q=taipei&appid=b1ecbccd638b763d489602917ba47cc3&units=metric&lang=zh_TW'
 
 res = requests.get(url)
 
 result = res.json()
 
+temp = []
+
 for data in result['list']:
-    print(data)
+    # print(data['main']['temp'])
+    temp.append(data['main']['temp'])
+
+plt.rc('font', family='Microsoft Jhenghei')
+
+plt.plot(temp,marker='.', markersize='6')
+plt.ylim(0,50)
+
+plt.title('天氣預報')
+plt.ylabel('溫度(攝氏)')
+
+plt.show()
