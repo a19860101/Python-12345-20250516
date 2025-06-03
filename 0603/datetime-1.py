46df3f2d0d319b3caeb71f2eb0b1451700d5eded
Zac <a19860101@gmail.com>
2025-06-03 15:21:16 +0800

    20250603

---
+++ 0603/datetime-1.py
@@ -0,0 +1,20 @@
+# 1748930400
+# 時間戳 timestamp
+
+import datetime
+
+# print(datetime.datetime.today())
+
+d = datetime.datetime.today()
+
+# d = datetime.datetime.fromtimestamp(1748930400)
+
+print(d)
+print(d.year)
+print(d.month)
+print(d.day)
+print(d.weekday())
+print(d.isoweekday())
+print(d.hour)
+print(d.minute)
+print(d.second)
--- 0603/matplotlib-plot-5.py
+++ 0603/matplotlib-plot-5.py
@@ -1,5 +1,6 @@
 import requests
 import matplotlib.pyplot as plt
+import datetime
 
 url = 'https://api.openweathermap.org/data/2.5/forecast?q=taipei&appid=b1ecbccd638b763d489602917ba47cc3&units=metric&lang=zh_TW'
 
