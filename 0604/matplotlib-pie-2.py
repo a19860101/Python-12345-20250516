4f50aa994126fd2be4a8e18a8722d42ac3232861
Zac <a19860101@gmail.com>
2025-06-04 14:44:42 +0800

    20250604

--- 0604/matplotlib-pie-2.py
+++ 0604/matplotlib-pie-2.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
 data = pd.read_csv('./travel.csv', header=1)
+print(data)
 
-d = data.loc[0]
+asia = data['Asia'].loc[0]
+total = data['Total'].loc[0]
+other = asia - total
 
-print(d)
+label = ['日本','南韓','香港','中國','澳門','其他國家']
