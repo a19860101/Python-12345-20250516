1534a245a4fd60dc24db7aed19f2c57cb6b5a5d8
Zac <a19860101@gmail.com>
2025-06-04 15:21:44 +0800

    20250604

--- 0604/matplotlib-pie-2.py
+++ 0604/matplotlib-pie-2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('./travel.csv', header=1)
 print(data)
@@ -8,3 +9,12 @@ total = data['Total'].loc[0]
 other = asia - total
 
 label = ['日本','南韓','香港','中國','澳門','其他國家']
+color = ['red','green','blue','purple','orange','pink']
+n = list(data.loc[0][2:7])
+n.append(other)
+
+plt.rc('font', family='Microsoft Jhenghei')
+plt.pie(n,labels=label, autopct='%.1f%%', radius=1.2, colors=color)
+
+plt.legend(bbox_to_anchor=(1,1))
+plt.show()
