584b80791535ff82f2a0a91f3a16304c519255f3
Zac <a19860101@gmail.com>
2025-06-04 10:38:25 +0800

    20250604

--- 0604/matplotlib-plot-6.py
+++ 0604/matplotlib-plot-6.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
-data = pd.read_csv('./STOCK_DAY_2330_202505.csv', encoding='big5')
+data = pd.read_csv('./STOCK_DAY_2330_202505.csv', encoding='big5', header=1)
+print(data.columns)
 # print(data['日期'])
-print(data['日期'],data['收盤價'])
+print(data['收盤價'][:20])
+
