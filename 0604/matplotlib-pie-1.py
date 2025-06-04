c7cdd2ef463c280821ccec36acff893180b620e9
Zac <a19860101@gmail.com>
2025-06-04 13:39:33 +0800

    20250604

--- 0604/matplotlib-pie-1.py
+++ 0604/matplotlib-pie-1.py
@@ -2,7 +2,14 @@ import matplotlib.pyplot as plt
 
 data = [10,20,30,40]
 label = ['A','B','C','D']
+e = [.1,0,0,0]
 
-plt.pie(data, radius=1.2, labels=label)
+plt.pie(data,
+        radius=1.2,
+        labels=label,
+        labeldistance=1.1,
+        startangle=15,
+        explode=e
+        )
 
 plt.show()
