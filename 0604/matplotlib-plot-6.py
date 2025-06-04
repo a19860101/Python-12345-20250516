e79cb278f46b75ce3b11a637eca8550e1b30ef7e
Zac <a19860101@gmail.com>
2025-06-04 11:30:08 +0800

    20250604

--- 0604/matplotlib-plot-6.py
+++ 0604/matplotlib-plot-6.py
@@ -6,14 +6,23 @@ data = pd.read_csv('./STOCK_DAY_2330_202505.csv', encoding='big5', header=1)
 # print(data['日期'])
 # print(data['收盤價'][:20])
 
-d = list(data['收盤價'][:20])
-d2 = list(data['開盤價'][:20])
-print(d)
-print(d2)
+close_ = list(data['收盤價'][:20])
+open_ = list(data['開盤價'][:20])
+max_ = list(data['最高價'][:20])
+min_ = list(data['最低價'][:20])
 
-plt.plot(d, marker='.', markersize=8)
-plt.plot(d2,marker='.', markersize=8, color='red')
+open_ = [ float(i.replace(',','')) for i in open_]
+max_ = [ float(i.replace(',','')) for i in max_]
 
-plt.ylim(800,1100)
+plt.rc('font', family='Microsoft Jhenghei')
+
+plt.plot(close_, marker='.', markersize=8, linewidth=1, label='收盤價')
+plt.plot(open_,marker='.', markersize=8, linewidth=1,label='開盤價')
+plt.plot(max_,marker='.', markersize=8, linewidth=1,label='最高價')
+plt.plot(min_,marker='.', markersize=8, linewidth=1,label='最低價')
+
+plt.ylim(900,1100)
+
+plt.legend()
 
 plt.show()
