4328815bf4b4a856f042f6133e87e6c3b996979a
Zac <a19860101@gmail.com>
2025-06-06 09:39:49 +0800

    20250606

--- 0606/matplotlib-pie-3.py
+++ 0606/matplotlib-pie-3.py
@@ -4,5 +4,17 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('./animal.csv')
 
 # print(data.columns)
-print(data['animal_kind'])
-print(data['animal_Variety'])
+# print(data['animal_kind'])
+# print(data['animal_Variety'])
+
+# total = len(data)
+total, col = data.shape
+
+dog = data[data['animal_kind'].str.contains('狗')]
+cat = data[data['animal_kind'].str.contains('貓')]
+
+dog_n = len(dog)
+cat_n = len(cat)
+other_n = total - dog_n - cat_n
+
+print(other_n)
