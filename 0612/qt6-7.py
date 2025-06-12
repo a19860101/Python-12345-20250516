f62cc8e3dcef92bcf2f925cfba599dad31929c41
Zac <a19860101@gmail.com>
2025-06-12 13:40:29 +0800

    20250612

--- 0612/qt6-7.py
+++ 0612/qt6-7.py
@@ -4,7 +4,7 @@ import sys
 app = QtWidgets.QApplication(sys.argv)
 
 widget = QtWidgets.QWidget()
-widget.resize(600,400)
+widget.resize(300,300)
 widget.setWindowTitle('盎司毫升轉換器')
 
 label1 = QtWidgets.QLabel(widget)
@@ -13,12 +13,26 @@ label1.setText('請輸入轉換的容量:')
 label2 = QtWidgets.QLabel(widget)
 label2.setText('請選擇換算單位:(oz/cc)')
 
+resultLabel = QtWidgets.QLabel(widget)
+resultLabel.setText('換算結果')
+
 input1 = QtWidgets.QLineEdit(widget)
 
 input2 = QtWidgets.QComboBox(widget)
 input2.addItems(['oz','cc'])
 
-btn = QtWidgets.QPushButton('換算')
+btn = QtWidgets.QPushButton(widget)
+btn.setText('換算')
+
+grid = QtWidgets.QGridLayout(widget)
+
+grid.addWidget(label1,1,1)
+grid.addWidget(label2,2,1)
+grid.addWidget(input1,1,2)
+grid.addWidget(input2,2,2)
+grid.addWidget(btn, 3,1, 1, 2)
+grid.addWidget(resultLabel, 4, 1)
+
 
 widget.show()
 sys.exit(app.exec())
